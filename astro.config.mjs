5d6bdf09e0aab98216ed27c8267b0c01552d55b1
Luis E Gonzalez <luego1986@gmail.com>
2025-06-24 17:50:43 -0500

    feat: add sitemap integration and robots.txt route

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
-
+  integrations: [svelte(), sitemap()],
+  site: 'https://texttoolbox.app',
   vite: {
     plugins: [tailwindcss()]
   }
